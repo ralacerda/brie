cd9f2eb002b7b1a69c660141a82c5141dd6a7951
Renato Lacerda <renatolacerda@pecege.com>
2024-06-25 13:10:50 -0300

    feat: animated background for loading button

--- lib/BrieButton/BrieButton.vue
+++ lib/BrieButton/BrieButton.vue
@@ -23,11 +23,12 @@ defineProps<{
   position: relative;
   background-color: var(--indigo-7);
   border: none;
-  border-radius: calc(var(--radius-2) - 1px);
-  color: var(--indigo-1);
+  border-radius: calc(var(--radius-2));
+  color: var(--indigo-0);
   cursor: pointer;
-  font-size: var(--font-size-1);
-  padding: var(--size-2) var(--size-3);
+  font-weight: var(--font-weight-6);
+  font-size: var(--font-size-2);
+  padding: var(--size-2) var(--size-4);
   transition: background-color 0.2s;
 
   &:hover {
@@ -49,23 +50,14 @@ defineProps<{
 
   &[aria-busy="true"] {
     cursor: progress;
-    background-color: var(--indigo-9);
-  }
-
-  &::before {
-    z-index: -1;
-    content: "";
-    position: absolute;
-    inset: -2px;
-    border-radius: var(--radius-2);
-    background-color: var(--indigo-7);
-  }
-
-  &[aria-busy="true"]::before {
-    background-image: linear-gradient(90deg, var(--gray-5), var(--gray-9));
-    background-size: 200%;
-    background-position: top left;
-    animation: spin 1s infinite ease-in-out;
+    animation: spin 1s ease-in-out infinite;
+    background: linear-gradient(
+      to right,
+      var(--indigo-9) 50%,
+      var(--indigo-7) 50%,
+      var(--indigo-9) 90%
+    );
+    background-size: 200% 100%;
   }
 }
 
